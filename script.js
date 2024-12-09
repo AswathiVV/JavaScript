6e0b8c14c38e462feeb0614fd1b0918aa4665cfa
AswathiVV <aswathi.v.v.916@gmail.com>
2024-12-09 13:28:07 +0530

    JS

---
+++ script.js
@@ -0,0 +1,69 @@
+// alert("WELCOME")
+
+// a="HELLO"
+// console.log("WELCOME",a)
+
+// a="HELLO"
+// alert(a)
+
+
+// var a="hello"
+// var a=20
+// alert(a)
+
+// const a='hello'
+// console.log(a);
+
+// let a='hiii'
+// a=10
+// alert(a)
+
+// let a
+// console.log(typeof a);               // undefined
+// console.log(a);
+
+// let a
+// a=10
+// console.log(typeof a);               // number
+// console.log(a);
+
+// let a
+// a="str"
+// console.log(typeof a);               // string
+// console.log(a);
+
+// let a
+// a=false
+// console.log(typeof a);               // boolean
+// console.log(a);
+
+// let a={name:'anu',age:20}
+// console.log(typeof a);
+// console.log(a);
+
+
+// console.log(10+5);
+// let a=5                          //   -,*,/,%,**,++,--
+// ++a
+// console.log(a);
+
+// let a=5
+// a++
+// console.log(a);
+
+
+// let a=10
+// a*=10                            // **==,*=,%=,**=,/=,+=,-=
+// console.log(a);
+
+// let a=20
+// let b=10
+// console.log(a!==b)          // ===,!==,==,<,>,!=
+// console.log(a!=b)
+// console.log(a===b)
+
+// let a=20
+// let b=10
+// console.log(a!=b && a==10);     // and  
+// console.log(a!=b || a==10);     // not
+
